a9d1f89bffe8fc13ddc347579fd6c017225acc45
Matt McNally <mgmcnally@umass.edu>
2015-11-06 22:11:47 -0500

    added command line arg for game stats

--- random_game_stats.py
+++ random_game_stats.py
@@ -1,10 +1,19 @@
 from __future__ import division
+import sys
+
 import random_game
+
 
 white_wins = 0
 black_wins = 0
+num_games = 10
 
-for i in range(10):
+# change num_games to command line arg if entered
+if(len(sys.argv) is 2):
+    num_games = int(sys.argv[1])
+
+print(num_games)
+for i in range(num_games):
     winner = random_game.run_random_game_no_printing()
     if winner is random_game.WHITE:
         white_wins += 1
