26511dbe645f7ac2f8e08d1bbf856021e48559f1
Matt McNally <mgmcnally@umass.edu>
2015-11-14 15:59:24 -0500

    refactored slightly and added main

--- minimax.py
+++ minimax.py
@@ -3,8 +3,8 @@ import sys
 import copy
 
 def minimax(board, move, depth, isMax):
-    if(depth > 0):
-        if(isMax):
+    if depth > 0:
+        if isMax:
             best_value, best_move = (sys.maxsize*-1,None)
         else:
             best_value, best_move = (sys.maxsize,None)
@@ -12,12 +12,10 @@ def minimax(board, move, depth, isMax):
             board_cpy = copy.deepcopy(board)
             board_cpy.push(legal_move)
             next_value = minimax(board_cpy,legal_move,depth-1,not isMax)[0]
-            if(isMax):
-                if(next_value > best_value):
-                    best_value, best_move = next_value, legal_move
-            else:
-                if(next_value < best_value):
-                    best_value, best_move = next_value, legal_move
+            if isMax and next_value > best_value:
+                best_value, best_move = next_value, legal_move
+            elif next_value < best_value:
+                best_value, best_move = next_value, legal_move
         return best_value, best_move
     else:
         return evaluate(board), move
@@ -26,29 +24,31 @@ def evaluate(board):
     sum = 0
     for i in range(64):
         piece = board.piece_at(i)
-        if(piece == 'p'):
+        if piece == 'p':
             sum -= 1
-        elif(piece == 'n' or piece == 'b'):
+        elif piece == 'n' or piece == 'b':
             sum -= 3
-        elif(piece == 'r'):
+        elif piece == 'r':
             sum -= 5
-        elif(piece == 'q'):
+        elif piece == 'q':
             sum -= 9
-        elif(piece == 'P'):
+        elif piece == 'P':
             sum += 1
-        elif(piece == 'N' or piece == 'B'):
+        elif piece == 'N' or piece == 'B':
             sum += 3
-        elif(piece == 'R'):
+        elif piece == 'R':
             sum += 5
-        elif(piece == 'Q'):
+        elif piece == 'Q':
             sum += 9
     return sum
 
-test_board = chess.Board()
 
-for i in range(4):
-    value, move = minimax(test_board,None,4,test_board.turn)
-    test_board.push(move)
-    print(move)
-    print(test_board)
-    print()
+if __name__=='__main__':
+    test_board = chess.Board()
+
+    for i in range(4):
+        value, move = minimax(test_board,None,2,test_board.turn)
+        test_board.push(move)
+        print(move)
+        print(test_board)
+        print()
