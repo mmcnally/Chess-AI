426d1cc19d913601a33d5f5ab5df34709ae6f125
Matt McNally <mgmcnally@umass.edu>
2015-11-15 14:56:38 -0500

    db tools can create db, table, and indices

---
+++ db_tools.py
@@ -0,0 +1,27 @@
+import os
+import sqlite3
+
+STATIC_PATH = os.getcwd()
+database = STATIC_PATH+'/db/datadump.db'
+
+
+''' sets up database if it isn't already set up '''
+def setup_db():
+    conn = sqlite3.connect('db/chess.db')
+    c = conn.cursor()
+
+    ''' create table if it isn't there '''
+    c.execute("CREATE TABLE IF NOT EXISTS Moves(" +
+              "Board STRING NOT NULL, " +
+              "Move STRING NOT NULL, " +
+              "Wins INT NOT NULL, " +
+              "TotalSeen INT NOT NULL)")
+
+    ''' create indices for table if they aren't there '''
+    c.execute("CREATE INDEX IF NOT EXISTS board_index ON Moves (Board)")
+    c.execute("CREATE INDEX IF NOT EXISTS move_index ON Moves (Move)")
+
+
+
+if __name__=='__main__':
+    setup_db()
