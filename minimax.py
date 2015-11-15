c25540f7d192a25f2f8a72ba4538fac8f26efefc
Matt McNally <mgmcnally@umass.edu>
2015-11-14 22:59:14 -0500

    added main to minimax file

--- minimax.py
+++ minimax.py
@@ -60,6 +60,6 @@ def minimax(board, move, depth, isMax):
         return evaluator.evaluate(board), move
 
 
-
-board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
-print(evaluator.evaluate(board))
+if __name__=='__main__':
+    board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
+    print(evaluator.evaluate(board))
