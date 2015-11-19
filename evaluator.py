4b5f96fab56a75ec38050de7e5d68f468aeb269c
esimon324 <eric.simon324@gmail.com>
2015-11-19 10:11:36 -0500

    new evaluation function implemented and put in evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -1,27 +1,23 @@
 import chess
 
-
 def evaluate(board):
     sum = 0
     for i in range(64):
-        pieceObj = board.piece_at(i)
-        piece = ""
-        if(pieceObj != None):
-            piece = pieceObj.symbol()
-        if piece == 'p':
-            sum -= 1
-        elif piece == 'n' or piece == 'b':
-            sum -= 3
-        elif piece == 'r':
-            sum -= 5
-        elif piece == 'q':
-            sum -= 9
-        elif piece == 'P':
-            sum += 1
-        elif piece == 'N' or piece == 'B':
-            sum += 3
-        elif piece == 'R':
-            sum += 5
-        elif piece == 'Q':
-            sum += 9
+        piece = board.piece_at(i)
+        if(piece != None):
+            if(piece.color == chess.WHITE):
+                sum += piece.piece_type
+            else:
+                sum -= piece.piece_type
+    return sum
+
+def evaluate2(board):
+    sum = 0
+    for i in range(64):
+        piece = board.piece_at(i)
+        if(piece != None):
+            if(piece.color == chess.WHITE):
+                sum += piece.piece_type + (len(board.attacks(i))*.025)
+            else:
+                sum -= piece.piece_type + (len(board.attacks(i))*.025)
     return sum
