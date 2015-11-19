e167c566e700cb1d6c44168bce90a308970a8b83
esimon324 <eric.simon324@gmail.com>
2015-11-19 10:48:59 -0500

    evaluate2 now returns max possible value on a checkmate. Maybe someone will actually win now

--- evaluator.py
+++ evaluator.py
@@ -1,4 +1,5 @@
 import chess
+import sys
 
 def evaluate(board):
     sum = 0
@@ -13,6 +14,11 @@ def evaluate(board):
 
 def evaluate2(board):
     sum = 0
+    if board.is_checkmate():
+        if board.turn == chess.BLACK:
+            return sys.maxsize
+        else:
+            return sys.maxsize*-1
     for i in range(64):
         piece = board.piece_at(i)
         if(piece != None):
